d61784d27bfc6156d074981fe90e1820e68ae03b
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-09-15 11:23:23 -0300

    juntando os códigos

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-<<<<<<< HEAD
 const checkBoxAgreement = document.querySelector('#agreement');
 const submitBtn = document.querySelector('#submit-btn');
+const counter = document.querySelector('#counter');
+const textArea = document.querySelector('#textarea');
+const li1 = document.querySelector('formData1');
+const li2 = document.querySelector('formData2');
+const li3 = document.querySelector('formData3');
+const li4 = document.querySelector('formData4');
+const li5 = document.querySelector('formData5');
+const li6 = document.querySelector('formData6');
+const li7 = document.querySelector('formData7');
 
 // Requisito 18
+// w3schools = For radiobuttons and checkboxes, the onchange event occurs when the checked state has been changed.
 checkBoxAgreement.onchange = function activeButton() {
   if (this.checked) {
     submitBtn.disabled = false;
@@ -10,18 +19,12 @@ checkBoxAgreement.onchange = function activeButton() {
     submitBtn.disabled = true;
   }
 };
-=======
-const button = document.querySelector('.buttonSubmit');
-const email = document.getElementById('emailHeader');
-const password = document.getElementById('passwordHeader');
 
-function validateForm() {
-  if (email.value === 'tryber@teste.com' && password.value === '123456') {
-    alert('Olá, Tryber!');
-  } else {
-    alert('Email ou senha inválidos.');
-  }
-}
-
-button.addEventListener('click', validateForm);
->>>>>>> aba54d251fa20a48d1a426828b02444031639287
+// Requisito 20
+// w3schools = The target event property returns the element that triggered the event.
+textArea.addEventListener('input', (e) => {
+  const { target } = e;
+  const maxLength = target.getAttribute('maxlength');
+  const currentLength = target.value.length;
+  counter.innerHTML = `${maxLength - currentLength}`;
+});
