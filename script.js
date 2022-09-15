2e97128a7591427b8f5c604ca119680dd07563d6
Stariel Isaac <starielfernandes@gmail.com>
2022-09-15 11:05:43 -0300

    requisitos até o 11 OK

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.querySelector('.buttonSubmit');
 const email = document.getElementById('emailHeader');
 const password = document.getElementById('passwordHeader');
 
+// requisito1 - validar se o possuem email e senha corretos
 function validateForm() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
